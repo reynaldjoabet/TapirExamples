5e16d1bcdd8e72d4c411f807c72f15fc8a244ae6
Liyeuk Reynald <reynaldjoabet@gmail.com>
2024-02-17 21:02:45 +0400

    domains modeling requests

---
+++ src/main/scala/http/requests/DeleteAccountRequest.scala
@@ -0,0 +1,14 @@
+
+package http
+package requests
+import io.circe.generic.semiauto.deriveCodec
+import io.circe.Codec
+final case class DeleteAccountRequest(
+    email: String,
+    password: String
+) 
+
+
+object DeleteAccountRequest{
+    implicit val deleteAccountRequestCodec: Codec.AsObject[DeleteAccountRequest]= deriveCodec[DeleteAccountRequest]
+}
---
+++ src/main/scala/http/requests/RegisterUserAccount.scala
@@ -0,0 +1,13 @@
+
+package http
+package requests
+import io.circe.generic.semiauto.deriveCodec
+import io.circe.Codec
+final case class RegisterUserAccount(
+    email: String,
+    password: String
+) 
+
+object RegisterUserAccount{
+    implicit val registerUserAccountCodec: Codec.AsObject[RegisterUserAccount]= deriveCodec[RegisterUserAccount]
+}
